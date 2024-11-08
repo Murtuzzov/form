b174911503b7492bb2561ccd1c90a0e683d51898
murtuzzov <murtuzzov>
2024-11-08 15:48:32 +0400

    Remarque

--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -5,11 +5,13 @@ interface ProgressBarProps {
 }
 
 const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => (
-  <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4 fixed top-0 left-0 right-0 z-10">
-    <div
-      className="bg-green-500 h-2.5 rounded-full transition-all duration-300"
-      style={{ width: `${progress}%` }}
-    ></div>
+  <div className="fixed left-1/2 transform -translate-x-1/2 top-4 w-full px-4 sm:px-8 max-w-lg">
+    <div className="w-full bg-gray-200 rounded-full h-2.5">
+      <div
+        className="bg-green-500 h-2.5 rounded-full transition-all duration-300"
+        style={{ width: `${progress}%` }}
+      ></div>
+    </div>
   </div>
 );
 
